dba2750cdbb39c4b80b0eb9a6854fcd16b52fb07
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-11-29 08:28:11 -0600

    commit

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.get(' / ', (req, res) => {
-    res.send('Hello')
+app.get('/api/users', (req, res) => {
+    res.status(200).json({message:'Welcome to the Support Desk API'})
 })
 
 app.listen(PORT, () => console.log(`server started on PORT ${PORT}` ))
